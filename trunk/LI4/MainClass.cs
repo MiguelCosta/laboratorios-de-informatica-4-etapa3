10152fca0bdc0aba421e2577cb762c6c6e3df9b5
miguelpintodacosta <miguelpintodacosta@f8afc5fa-26ec-bbfd-8d5c-6cb8fcb06e7e>
2011-05-23 21:24:01 +0000

    Repositório Iniciado

---
+++ trunk/LI4/MainClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LI4
+{
+    class MainClass
+    {
+        static void Main()
+        {
+
+            User u = new User("Miguel", "Miguel@mail.um", "83");
+            Console.WriteLine("Nome: "+u.Username);
+            Console.WriteLine("Email: "+u.Email);
+            Console.WriteLine("Password: " + u.Password);
+
+            u.Username = "Miguel";
+            u.Email = "email2";
+            u.Password = "p2";
+
+            User u2 = new User(u);
+            u2.Password = "qualquer coisa";
+
+            Console.WriteLine("Nome: " + u2.Username);
+            Console.WriteLine("Email: " + u2.Email);
+            Console.WriteLine("Password: " + u2.Password);
+
+            Console.WriteLine("User: "+u2.equals(u));
+        }
+    }
+}
---
+++ trunk/LI4/User.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LI4
+{
+    class User
+    {
+        private string _username;
+        private string _email;
+        private string _password;
+
+        /**
+         * Constructor default
+         * */
+        public User()
+        {
+            _username = "";
+            _email = "";
+            _password = "";
+        }
+
+        /**
+         * Constructor with parameters
+         * */
+        public User(string username, string email, string password)
+        {
+            _username = username;
+            _email = email;
+            _password = password;
+        }
+
+        /**
+         * Constructor with User
+         * */
+        public User(User u)
+        {
+            _username = u.Username;
+            _email = u.Email;
+            _password = u.Password;
+        }
+
+        /**
+         * Methods get and set
+         * */
+        public string Username
+        {
+            get { return _username;  }
+            set { _username = value; }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        /**
+         * Method clone
+         * */
+        public User clone()
+        {
+            return new User(this);
+        }
+
+        public bool equals(Object o)
+        {
+            if (this == o) return true;
+            if (o == null || o.GetType()!= this.GetType()) return false;
+
+            User u = (User) o;
+            if (_username.Equals(u.Username) && _email.Equals(u.Email) && _password.Equals(u.Password)) return true;
+
+            return false;
+        }
+
+        public string toString()
+        {
+            StringBuilder s = new StringBuilder("User\n");
+            s.Append(_username);
+            s.Append("\n");
+            s.Append(_email);
+            return s.ToString();
+        }
+
+    }
+}
